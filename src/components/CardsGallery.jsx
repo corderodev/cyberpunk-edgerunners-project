c30eb2d73358ecf5914837d82fae0bebc93e9c8b
Daniel Cordero <dn.lambb@gmail.com>
2023-01-29 15:17:50 -0300

    feat(component): add new cards gallery
    Add two types of cards: image card and video card for links to youtube

---
+++ src/components/CardsGallery.jsx
@@ -0,0 +1,22 @@
+import { BtnPlay } from './BtnPlay';
+
+const CardVideo = ({ img, alt, link }) => {
+  return (
+    <div className="gallery-section__cards-container--card-video">
+      <span className='corner-transparent'></span>
+      <img className="card--img" src={img} alt={alt} />
+      <BtnPlay link={link} />
+    </div>
+  );
+};
+
+const CardImage = ({ img, alt }) => {
+  return (
+    <div className="gallery-section__cards-container--card-img">
+      <span className='corner-transparent'></span>
+      <img className="card--img" src={img} alt={alt} />
+    </div>
+  );
+};
+
+export { CardVideo, CardImage };
