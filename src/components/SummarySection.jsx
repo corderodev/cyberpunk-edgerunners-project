23b3b2f5a28fed81a544c0d965055e73b629a89d
Daniel Cordero <dn.lambb@gmail.com>
2023-02-03 18:24:07 -0300

    feat: add corner transparent for images in Summary

--- src/components/SummarySection.jsx
+++ src/components/SummarySection.jsx
@@ -1,5 +1,5 @@
 import summary1 from '../assets/img/summary-1.webp';
-import summary2 from '../assets/img/summary-2.webp';
+import summary2 from '../assets/img/night-city.webp';
 
 export const SummarySection = () => {
   return (
@@ -10,9 +10,11 @@ export const SummarySection = () => {
       </p>
       <div className="summary-section__img-container">
         <div className="summary-section__img-container--img-left">
+          <span className="corner-transparent"></span>
           <img src={summary1} alt="Lucy and David" />
         </div>
         <div className="summary-section__img-container--img-right">
+          <span className="corner-transparent"></span>
           <img src={summary2} alt="Night City" />
         </div>
       </div>
