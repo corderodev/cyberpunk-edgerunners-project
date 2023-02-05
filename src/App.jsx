fb78830fe4af234505c927ee859543257734578b
Daniel Cordero <dn.lambb@gmail.com>
2023-02-04 21:15:45 -0300

    feat(component): add new footer section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Gallery } from './components/Gallery';
 import { Glossary } from './components/Glossary';
 import { NewsLetter } from './components/NewsLetter';
 import { Social } from './components/Social';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Glossary />
       <NewsLetter />
       <Social />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <a
+        href="https://www.cyberpunk.net/en/edgerunners"
+        className="footer__link-page"
+        target="_blank">
+        View original Cyberpunk Edgerunners page
+      </a>
+    </footer>
+  );
+};
